d8bc9d2bdec7713e59fe252df55fe425a8c9c5c1
Higor Diego <higordiegoti@gmail.com>
2018-06-29 13:05:02 -0300

    finish

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('fs');
-const ytdl = require('ytdl-core');
 const path = require('path')
+const ytdl = require('ytdl-core');
 const { promisify } = require('util')
 
-const infoVideo = promisify(ytdl.getInfo)
+const getInfoVideo = promisify(ytdl.getInfo)
 
 const app = express()
 
-app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'videos')))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
+
 app.post('/download', async (req, res) => {
     try {
-        const info = await infoVideo(req.body.url.replace('https://www.youtube.com/watch?v=', ''))
+        const info = await getInfoVideo(req.body.url.replace('https://www.youtube.com/watch?v=', ''))
         ytdl(req.body.url)
             .pipe(fs.createWriteStream(`videos/${info.title}.mp4`))
-            .on('finish', () => res.status(200).json({video: `${info.title}.mp4`}))
+            .on('finish', () => res.status(200).json({ video: `${info.title}.mp4` }))
     } catch (err) {
         res.status(500).json(err)
     }
